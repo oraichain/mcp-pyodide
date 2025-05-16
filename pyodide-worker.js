db4f1cc0e0836ee20d7d377b9ed9d013d4386c6a
Pham Tu <tubackkhoa@gmail.com>
2025-05-16 15:17:26 +0700

    add declaration in js

--- pyodide-worker.js
+++ pyodide-worker.js
@@ -1,27 +1,28 @@
-import fs from "fs";
-import { workerData, parentPort } from "worker_threads";
-import { PyodideManager } from "./build/lib/pyodide/pyodide-manager.js";
+import fs from 'fs';
+import { workerData, parentPort } from 'worker_threads';
+import { PyodideManager } from './build/lib/pyodide/pyodide-manager.js';
 
-const TEST_CACHE_DIR = "./cache";
+const TEST_CACHE_DIR = './cache';
 
 // run code
-if (workerData.cmd === "runCode") {
+if (workerData.cmd === 'runCode') {
   if (!workerData.sessionId) {
-    throw new Error("sessionId is required");
+    throw new Error('sessionId is required');
   }
+  /** @type PyodideManager */
   const pyodideManager = new PyodideManager(workerData.sessionId);
   // first time init
   const isInitialized = await pyodideManager.initialize(TEST_CACHE_DIR);
   if (!isInitialized) {
-    throw new Error("Failed to initialize Pyodide");
+    throw new Error('Failed to initialize Pyodide');
   }
   const mountResult = await pyodideManager.mountDirectory();
   pyodideManager.chdir();
   if (!mountResult) {
-    throw new Error("Failed to mount directory");
+    throw new Error('Failed to mount directory');
   }
 
   await pyodideManager.installPackages(workerData.code);
   const response = await pyodideManager.executePython(workerData.code);
-  parentPort.postMessage({ cmd: "response", result: response });
+  parentPort.postMessage({ cmd: 'response', result: response });
 }
